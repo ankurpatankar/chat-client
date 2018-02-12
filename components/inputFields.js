42b9d32f7b5d75445e4d1130ceb2dd1514dddcd7
Ankur Patankar <ankur.patankar@gmail.com>
2018-02-11 23:13:35 -0800

    create component for input fields

---
+++ components/inputFields.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class SingleLine extends React.Component {
+    render () {
+        const {
+            label,
+            name,
+            onChange,
+            value,
+        } = this.props;
+        return (
+            <div className="input-field">
+                {
+                    label
+                        ? <label>{label}</label>
+                        : null
+                }
+                <input
+                        type="text"
+                        value={value}
+                        name={name}
+                        onChange={e => onChange(e)} />
+            </div>
+        );
+    }
+}
+
+class MultiLine extends React.Component {
+    render () {
+        const {
+            label,
+            name,
+            onChange,
+            value,
+        } = this.props;
+        return (
+            <div className="input-field">
+                {
+                    label
+                        ? <label>{label}</label>
+                        : null
+                }
+                <textarea
+                        autoCapitalize="sentences"
+                        value={value}
+                        name={name}
+                        onChange={e => onChange(e)}
+                        rows="4"
+                        columns="20" />
+            </div>
+        );
+    }
+}
+
+module.exports = {
+    SingleLine,
+    MultiLine,
+}
